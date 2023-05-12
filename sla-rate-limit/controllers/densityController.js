e756221fbb3c29becc42e52711dd4f845ff7c93c
pabperort1 <pabperort1@alum.us.es>
2023-05-12 12:38:11 +0200

    Funciona

---
+++ sla-rate-limit/controllers/densityController.js
@@ -0,0 +1,13 @@
+
+export function getDensitys(_, res) {
+    res.send([{"year": 2011,
+    "province": "Málaga",
+    }])
+}
+
+export function findByyear(_, res) {
+    res.send({"year": 2011,
+    "province": "Málaga",
+    })
+}
+
